89acb2634eabb60830d8acf60c7374d0b9f84865
lumigamher <lumigam@hotmail.com>
2024-01-18 15:48:29 -0500

    arregalndo visualizacion menu movil

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 // AOS.init({
 //     offset:1
 // });
+let nav = document.querySelector('nav');
+document.querySelector('.boton').addEventListener('click', function(){
+    nav.classList.toggle('active')
+});
